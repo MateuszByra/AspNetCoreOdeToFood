af9efacae19836f568bde4964813a9c774327a97
Mateusz Byra <mat.byra@gmail.com>
2017-04-17 17:57:52 +0200

    Add configuration file appsettings.json. Include it in ConfiguratuionBuilder to use values from this file.

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -8,6 +9,16 @@ namespace OdeToFood
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder() // configuration builder to add configuration file (appesettings.json here).
+                                .SetBasePath(env.ContentRootPath) // use root path from hosting environment
+                                .AddJsonFile("appsettings.json")
+                                .AddEnvironmentVariables();
+                Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,7 +37,8 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var message = Configuration["Greeting"];
+                await context.Response.WriteAsync(message);
             });
         }
     }
