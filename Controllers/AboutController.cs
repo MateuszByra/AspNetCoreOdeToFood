1701d1489a79713f9c8c2f3f6457f289a2d98960
Mateusz Byra <mat.byra@gmail.com>
2017-04-23 13:35:38 +0200

    Add Restaurant mogel and Index.cshtml view for Home controller, index action.

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -5,6 +5,7 @@ namespace OdeToFood.Controllers{
     [Route("[controller]/[action]")]
     public class AboutController{
 
+        //[Route("")] empty - About/
         public string Phone(){
             return "1+555-555-5555";
         }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
 
-namespace OdeToFood.Controllers{
+namespace OdeToFood.Controllers
+{
 
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            
-            return Content("Hello, from the HomeController.");
+            var model = new Restaurant { Id = 1, Name = "The House of Kobe." };
+
+            //return new ObjectResult(model); // object result return serialized model, eg. in json (if app content in header is json. Depends on content header).
+            return View();
         }
     }
 }
